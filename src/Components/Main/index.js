1ebacf1430afa1f2a1387f1d91246dd168c0d954
work inventions <dharakachhadia@gmail.com>
2022-12-01 18:06:48 +0530

    changes

---
+++ src/Components/Main/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles.scss";
+
+function Main() {
+  return (
+    <div className="main-container">
+      <div className="header-title">
+        <div>
+          <p>serchbar</p>
+        </div>
+        <div>
+          <p>date</p>
+        </div>
+        <div>
+          <p>bell</p>
+        </div>
+        <div>
+          <p>name</p>
+        </div>
+      </div>
+      <div className="header-main-container">
+        <div className="stock-market-container">
+          <p>serchbar</p>
+        </div>
+
+        <div className="portfolio-section">
+          <p>serchbar</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
---
+++ src/Components/Sidler/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss";
+function Sidler() {
+  return (
+    <div className="sidler-container">
+      <div className="sidler-title">STOCKIN</div>
+      <div className="sidler-tab">
+        <p>Market</p>
+        <p>Dashboard</p>
+        <p>Portfolio</p>
+        <p>news</p>
+        <p>Settings</p>
+      </div>
+    </div>
+  );
+}
+
+export default Sidler;
